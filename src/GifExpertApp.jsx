b079b805d073092cb4ed5707a65523394df5a2bd
Alberto Enriquez <136874006+EnriquezAlberto@users.noreply.github.com>
2023-08-14 19:36:42 -0300

    validacion de datos

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { useState } from "react"
+import { AddCategory } from "./components/AddCategory";
 
 export const GifExpertApp = () => {
+
+const [ categories, setCategories ] = useState(['One Punch', 'Dragon Ball']);
+
+const onAddCategory = ( newCategory) => {
+console.log(newCategory)
+//   setCategories( [NewCategory, ...categories])
+}
+
+    
   return (
     <>
+    {/* titulo */}
     <h1>GifExpertApp</h1>
+
+    {/* Input */}
+    <AddCategory 
+    // setCategories={ setCategories }
+    onNewCategory={ value => onAddCategory(value) }
+    />
+
+
+    {/* listado de Gift */}
+
+
+<ol>
+     { categories.map( category => {
+        return <li key={ category }>{ category }</li>
+     })}
+    <li>XYZ</li>
+</ol>
+
+          {/* Gif Item */}
     </>
   )
 }
