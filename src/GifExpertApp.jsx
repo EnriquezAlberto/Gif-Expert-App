1057ac8d6afdb94e7c53af040922ed986fa4896c
Alberto Enriquez <136874006+EnriquezAlberto@users.noreply.github.com>
2023-08-14 21:29:01 -0300

    fetching y mejoras

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,39 +1,36 @@
 import { useState } from "react"
 import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
 
-const [ categories, setCategories ] = useState(['One Punch', 'Dragon Ball']);
+const [ categories, setCategories ] = useState(['One Punch']);
 
 const onAddCategory = ( newCategory) => {
-console.log(newCategory)
-//   setCategories( [NewCategory, ...categories])
+if (categories.includes(newCategory)){
+    return;
+
+}
+  setCategories( [newCategory, ...categories])
 }
 
     
   return (
     <>
-    {/* titulo */}
     <h1>GifExpertApp</h1>
 
-    {/* Input */}
     <AddCategory 
-    // setCategories={ setCategories }
     onNewCategory={ value => onAddCategory(value) }
     />
 
-
-    {/* listado de Gift */}
-
-
-<ol>
-     { categories.map( category => {
-        return <li key={ category }>{ category }</li>
-     })}
-    <li>XYZ</li>
-</ol>
-
-          {/* Gif Item */}
+    { categories.map( (category) => 
+        (
+        <GifGrid 
+        key={category} 
+        category={category}
+        />
+    ))
+    }
     </>
   )
 }
